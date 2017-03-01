86973dd1085c3261d57b2146350b88b43e80534c
Colby M. White <colbywhite@users.noreply.github.com>
2017-02-28 20:06:22 -0800

    add tests for nested events

--- test/monitor-test.js
+++ test/monitor-test.js
@@ -21,8 +21,9 @@ describe('Monitor#monitorStack', function(){
   })
 
   statuses.forEach(function(state){
+    const action = state.split('_')[0];
+
     it(`should keep monitoring until ${state} stack status`, function() {
-      const action = state.split('_')[0];
       const inProgressEvent = {
         StackEvents: [
           {
@@ -58,6 +59,58 @@ describe('Monitor#monitorStack', function(){
           assert.equal(state, stackStatus);
           // 1 INFO at the beginning, then 1 for each event
           assert.equal(spylogger.spy.callCount, 3);
+        });
+    })
+
+    it(`should not stop monitoring on ${state} nested stack status`, function() {
+      const inProgressEvent = {
+        StackEvents: [
+          {
+            EventId: '1a2b3c4d',
+            StackName: STACK_NAME,
+            LogicalResourceId: STACK_NAME,
+            ResourceType: 'AWS::CloudFormation::Stack',
+            Timestamp: new Date(),
+            ResourceStatus: `${action}_IN_PROGRESS`
+          }
+        ]
+      };
+      const nestedStackEvent = {
+        StackEvents: [
+          {
+            EventId: '1e2f3g4z',
+            StackName: STACK_NAME,
+            LogicalResourceId: 'nested-stack-name',
+            ResourceType: 'AWS::CloudFormation::Stack',
+            Timestamp: new Date(),
+            ResourceStatus: state
+          },
+        ],
+      };
+      const completedEvent = {
+        StackEvents: [
+          {
+            EventId: '1e2f3g4h',
+            StackName: STACK_NAME,
+            LogicalResourceId: STACK_NAME,
+            ResourceType: 'AWS::CloudFormation::Stack',
+            Timestamp: new Date(),
+            ResourceStatus: state
+          }
+        ]
+      };
+
+      describeStackEventsAsyncStub.onCall(0).returns(Promise.resolve(inProgressEvent));
+      describeStackEventsAsyncStub.onCall(1).returns(Promise.resolve(nestedStackEvent));
+      describeStackEventsAsyncStub.onCall(2).returns(Promise.resolve(completedEvent));
+
+      return testMonitor.monitor({StackId: STACK_NAME})
+        .then(function(stackStatus) {
+          assert.equal(3, describeStackEventsAsyncStub.callCount);
+          assert.ok(describeStackEventsAsyncStub.calledWithExactly({StackName: STACK_NAME}));
+          assert.equal(state, stackStatus);
+          // 1 INFO at the beginning, then 1 for each event
+          assert.equal(spylogger.spy.callCount, 4);
         });
     })
   });
