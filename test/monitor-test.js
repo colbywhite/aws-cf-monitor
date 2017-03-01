4ac5564062f56400c379e43e9d661d22c340811c
Colby M. White <colbywhite@users.noreply.github.com>
2017-02-28 21:01:30 -0800

    add test for non stack events

--- test/monitor-test.js
+++ test/monitor-test.js
@@ -29,12 +29,12 @@ describe('Monitor#monitorStack', function(){
           LogicalResourceId: STACK_NAME,
           ResourceType: 'AWS::CloudFormation::Stack',
           Timestamp: new Date(),
-          ResourceStatus: 'DELETE_IN_PROGRESS',
+          ResourceStatus: 'DELETE_IN_PROGRESS'
         },
       ],
     };
     const stackNotFoundError = {
-      message: 'Stack new-service-dev does not exist',
+      message: 'Stack new-service-dev does not exist'
     };
 
     describeStackEventsAsyncStub.onCall(0).returns(Promise.resolve(deleteStartEvent));
@@ -47,6 +47,57 @@ describe('Monitor#monitorStack', function(){
         assert.equal('DELETE_COMPLETE', stackStatus);
         // 1 INFO at the beginning and the end, then 1 for each real event
         assert.equal(spylogger.spy.callCount, 3);
+      });
+  })
+
+  it('should keep monitoring when 1st event is not "AWS::CloudFormation::Stack"', function() {
+    const bucketEvent = {
+      StackEvents: [
+        {
+          EventId: '1a2b3c4d',
+          StackName: STACK_NAME,
+          LogicalResourceId: 'somebucket',
+          ResourceType: 'AWS::S3::Bucket',
+          Timestamp: new Date()
+        },
+      ],
+    };
+    const progressEvent = {
+      StackEvents: [
+        {
+          EventId: '1w2x3y4z',
+          StackName: STACK_NAME,
+          LogicalResourceId: STACK_NAME,
+          ResourceType: 'AWS::CloudFormation::Stack',
+          Timestamp: new Date(),
+          ResourceStatus: 'UPDATE_IN_PROGRESS'
+        },
+      ],
+    };
+    const completedEvent = {
+      StackEvents: [
+        {
+          EventId: '1m2n3o4p',
+          StackName: STACK_NAME,
+          LogicalResourceId: STACK_NAME,
+          ResourceType: 'AWS::CloudFormation::Stack',
+          Timestamp: new Date(),
+          ResourceStatus: 'UPDATE_COMPLETE'
+        },
+      ],
+    };
+
+    describeStackEventsAsyncStub.onCall(0).returns(Promise.resolve(bucketEvent));
+    describeStackEventsAsyncStub.onCall(1).returns(Promise.resolve(progressEvent));
+    describeStackEventsAsyncStub.onCall(2).returns(Promise.resolve(completedEvent));
+
+    return testMonitor.monitor({StackId: STACK_NAME})
+      .then(function(stackStatus) {
+        assert.equal(3, describeStackEventsAsyncStub.callCount);
+        assert.ok(describeStackEventsAsyncStub.calledWithExactly({StackName: STACK_NAME}));
+        assert.equal('UPDATE_COMPLETE', stackStatus);
+        // 1 INFO at the beginning and the end, then 1 for each real event
+        assert.equal(spylogger.spy.callCount, 5);
       });
   })
 
