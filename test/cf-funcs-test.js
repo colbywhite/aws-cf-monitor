9f69fa11659946a9cfc16dfcaea2782ac08f3d67
Colby M. White <colbywhite@users.noreply.github.com>
2017-07-19 23:01:04 -0500

    refactor happy path tests to include future cOrU func

--- test/cf-funcs-test.js
+++ test/cf-funcs-test.js
@@ -5,6 +5,12 @@ const spylogger = require('./spy-logger');
 const STACK_NAME = 'mock-stack'
 
 var cloudFormation = require('./mock-aws');
+const happyPathRuns = [
+  {func: funcs.createStack, action: 'CREATE', cfFuncName: 'createStackAsync'},
+  {func: funcs.updateStack, action: 'UPDATE', cfFuncName: 'updateStackAsync'},
+  {func: funcs.deleteStack, action: 'DELETE', cfFuncName: 'deleteStackAsync'},
+  {func: funcs.createOrUpdateStack, action: 'CREATE', cfFuncName: 'createStackAsync', descAddendum: 'when create is successful'}
+]
 
 describe('cf-funcs', function() {
   before(function() {
@@ -18,12 +24,14 @@ describe('cf-funcs', function() {
 
   afterEach(function() {
     describeStackEventsAsyncStub.restore();
+    cloudFormation.createStackAsync.reset();
+    cloudFormation.updateStackAsync.reset();
+    cloudFormation.deleteStackAsync.reset();
   })
 
-  Object.getOwnPropertyNames(funcs).forEach(function(func){
-    describe(`#${func}`, function(){
-      const action = func.replace('Stack', '').toUpperCase();
-      it('should wait for completion and log events', function() {
+  happyPathRuns.forEach(function(run){
+    describe(`#${run.func.name}`, function(){
+      it(`should wait for completion and log events ${run.descAddendum || ''}`, function() {
         const inProgressEvent = {
           StackEvents: [
             {
@@ -32,7 +40,7 @@ describe('cf-funcs', function() {
               LogicalResourceId: STACK_NAME,
               ResourceType: 'AWS::CloudFormation::Stack',
               Timestamp: new Date(),
-              ResourceStatus: `${action}_IN_PROGRESS`
+              ResourceStatus: `${run.action}_IN_PROGRESS`
             }
           ]
         };
@@ -44,19 +52,19 @@ describe('cf-funcs', function() {
               LogicalResourceId: STACK_NAME,
               ResourceType: 'AWS::CloudFormation::Stack',
               Timestamp: new Date(),
-              ResourceStatus: `${action}_COMPLETE`
+              ResourceStatus: `${run.action}_COMPLETE`
             }
           ]
         };
         describeStackEventsAsyncStub.onCall(0).returns(Promise.resolve(inProgressEvent));
         describeStackEventsAsyncStub.onCall(1).returns(Promise.resolve(completedEvent));
-        return funcs[func]({StackName: STACK_NAME}, cloudFormation)
+        return run.func({StackName: STACK_NAME}, cloudFormation)
           .then(function(finalStatus){
-            assert.equal(finalStatus, `${action}_COMPLETE`);
+            assert.equal(finalStatus, `${run.action}_COMPLETE`);
             assert.equal(describeStackEventsAsyncStub.callCount, 2);
             assert.ok(describeStackEventsAsyncStub.calledWithExactly({StackName: STACK_NAME}));
-            assert.equal(cloudFormation[`${func}Async`].callCount, 1);
-            assert.ok(cloudFormation[`${func}Async`].calledWithExactly({StackName: STACK_NAME}));
+            assert.equal(cloudFormation[run.cfFuncName].callCount, 1);
+            assert.ok(cloudFormation[run.cfFuncName].calledWithExactly({StackName: STACK_NAME}));
             // 1 INFO at the beginning and the end, then 1 for each event
             assert.equal(spylogger.callCount, 4);
           });
